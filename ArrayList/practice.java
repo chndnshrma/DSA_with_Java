d73652e4f4ba8707a60e475e022ae426cce11f4a
Chndn <chndnsharma5036@gmail.com>
2025-09-26 23:05:02 +0530

    adding new files

---
+++ ArrayList/practice.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class practice {
+    public static void main(String[] args){
+        ArrayList<Integer> num = new ArrayList<>();
+        for(int i = 0; i<10; i++){
+            num.add(i);
+        }
+
+        System.out.println(evenOdd(num));
+    }
+    public static ArrayList<ArrayList<Integer>> evenOdd(ArrayList<Integer> list) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+        
+        for(Integer x: list){
+            if(x % 2 == 0){
+                even.add(x);
+            }else{
+                odd.add(x);
+            }
+        }
+        result.add(even);
+        result.add(odd);
+        return result;
+    }
+}
