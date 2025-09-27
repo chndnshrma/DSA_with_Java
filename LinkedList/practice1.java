d311358fed51b880843d5919172631eab04fc05a
Chndn <chndnsharma5036@gmail.com>
2025-09-28 03:03:12 +0530

    learning time complexity

--- LinkedList/practice1.java
+++ LinkedList/practice1.java
@@ -1,1 +1,14 @@
-hello
+
+class practice1{
+    public static void main(String[] args) {
+        // Your code here
+        int n = 5;
+        int fact = 1;
+        for(int i =1; i<=n;i++){
+            fact *= i;
+            
+        }
+        System.out.print (fact);
+        System.out.println("\n");
+    }
+}
