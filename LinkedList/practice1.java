07f7f7fcc88cab34ede0ec554285f8e891bccbfc
Chndn <chndnsharma5036@gmail.com>
2025-10-01 01:44:26 +0530

    josephus problem

--- LinkedList/practice1.java
+++ LinkedList/practice1.java
@@ -1,14 +1,60 @@
+import java.util.Iterator;
+import java.util.LinkedList;
 
 class practice1{
     public static void main(String[] args) {
-        // Your code here
-        int n = 5;
-        int fact = 1;
-        for(int i =1; i<=n;i++){
-            fact *= i;
-            
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list.remove(20));
+    }
+    public static int josephor(int n, int k){
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for(int i = 0; i<n; i++){
+            list.add(i);
         }
-        System.out.print (fact);
-        System.out.println("\n");
+
+        Iterator<Integer> it = list.iterator();
+
+        while(list.size() > 1){
+            int count = 0;
+            while(count < k){
+                while(it.hasNext() && count < k){
+                    it.next();
+                    count++;
+                }
+                if(count < k){
+                    it = list.iterator();
+                    it.next();
+                    count++;
+                }
+            }
+            it.remove();
+        }
+        return list.getFirst();
+    }
+}
+
+class myDS{
+    LinkedList<Integer> list = new LinkedList<>();
+
+    void add(int x){
+        list.add(x);
+    }
+
+    void removeAndPrint(int x){
+        Iterator it = list.iterator();
+        while(it.hasNext()){
+            Integer n = (Integer)it.next();
+            if(n.equals(x)){
+                it.remove();
+            }else{
+                System.out.println(n + " ");
+            }
+        }
+        System.out.println();
     }
 }
