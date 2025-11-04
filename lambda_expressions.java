20a8a402be542fd0e0fcbc342396eee1f798c3f2
Chndn <chndnsharma5036@gmail.com>
2025-11-05 01:03:39 +0530

    lambda expr

--- lambda_expressions.java
+++ lambda_expressions.java
@@ -1,5 +1,12 @@
+import java.util.*;
+import java.io.*;
 public class lambda_expressions {
     public static void main(String[] args) {
-        
+        List<Integer> lst = new ArrayList<>();
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+
+        lst.stream().forEach(e -> System.out.println(e));
     }
 }
