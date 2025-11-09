5bc8bf8d1e3b6b56d997e927b436a39f941fd5dc
Chndn <chndnsharma5036@gmail.com>
2025-11-09 12:15:30 +0530

    basic stack operations

--- Stacks/src/Main.java
+++ Stacks/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayDeque;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        // Stack<Integer> stack = new Stack<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack.isEmpty());
     }
 }
