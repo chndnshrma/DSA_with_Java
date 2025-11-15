4b75de545563fa87a403d16c625a896f7c943921
Chndn <chndnsharma5036@gmail.com>
2025-11-15 17:00:46 +0530

    program to check is string has balanced paranthesis

---
+++ Stacks/src/Balance.java
@@ -0,0 +1,34 @@
+//This program check is the string has balanced parenthesis
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Balance {
+    public static void main(String[] args) {
+    String str = "({}[]))";
+        System.out.println(isBalanced(str));
+    }
+    public static boolean isBalanced(String str){
+        Deque<Character> stack = new ArrayDeque<Character>();
+
+        for(int i = 0; i<str.length(); i++){
+            char x = str.charAt(i);
+            if(x == '(' || x == '{' || x == '['){
+                stack.push(x);
+            }else {
+                if (stack.isEmpty()) {
+                    return false;
+                }else if(isMatching(stack.peek(), x) == false) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return (stack.isEmpty() == true);
+    }
+   public static boolean isMatching(char a, char b){
+        return (
+                a == '(' && b == ')' || a=='[' && b == ']'  || a=='{' && b == '}'
+                );
+    }
+}
