dbd5b127a4183993cad9d35dfca278202d727a00
Chndn <chndnsharma5036@gmail.com>
2025-11-15 23:20:46 +0530

    fixed error in span.java

--- Stacks/src/Span.java
+++ Stacks/src/Span.java
@@ -1,2 +1,13 @@
 public class Span {
+    public static void main(String[] args){
+        int[] arr = {18,12,13,14,11,16};
+
+        for(int i = 0;i < arr.length; i++){
+            int span = 1;
+            for(int j = i-1; j>=0 && arr[j] <= arr[i]; j--){
+                span++;
+            }
+            System.out.print(span + " ");
+        }
+    }
 }
