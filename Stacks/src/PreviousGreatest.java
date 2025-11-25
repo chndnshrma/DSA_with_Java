72c23341c83a11db8db1bf2d912d4ad5f22501a5
Chndn <chndnsharma5036@gmail.com>
2025-11-26 02:24:36 +0530

    using array

--- Stacks/src/PreviousGreatest.java
+++ Stacks/src/PreviousGreatest.java
@@ -1,15 +1,20 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.Iterator;
+import java.util.*;
 
 public class PreviousGreatest {
     public static void main(String[] args){
-        ArrayDeque<Integer> stack = new ArrayDeque<>(Arrays.asList(15,10,18,12,4,6,2,8));
-
-        Iterator<Integer> iterator = stack.iterator();
-        while(iterator.hasNext()){
-            System.out.print(iterator.next() + " ");
+        int[] stack = {15,10,18,12,4,6,2,8};
+        prevGreater(stack);
+    }
+    public static void prevGreater(int arr[]) {
+        for(int i = 0; i < arr.length; i++){
+            int pg = -1;
+            for(int j = i-1; j >= 0; j--) {
+                if (arr[j] > arr[i]) {
+                    pg = arr[j];
+                    break;
+                }
+            }
+            System.out.print(pg + " ");
         }
     }
 }
