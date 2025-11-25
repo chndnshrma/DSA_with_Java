33e3deff07d5c10adebcf0a23e0546041c810067
Chndn <chndnsharma5036@gmail.com>
2025-11-26 02:16:01 +0530

    iterating though stack to compare emelents

--- Stacks/src/PreviousGreatest.java
+++ Stacks/src/PreviousGreatest.java
@@ -1,5 +1,15 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Iterator;
+
 public class PreviousGreatest {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        ArrayDeque<Integer> stack = new ArrayDeque<>(Arrays.asList(15,10,18,12,4,6,2,8));
+
+        Iterator<Integer> iterator = stack.iterator();
+        while(iterator.hasNext()){
+            System.out.print(iterator.next() + " ");
+        }
     }
 }
