a1d309fd3c47b994d3b671c4a2d87baf4f49914b
Chndn <chndnsharma5036@gmail.com>
2025-11-27 22:13:52 +0530

    fixing error in arrays implementation

--- Stacks/src/PreviousGreatest.java
+++ Stacks/src/PreviousGreatest.java
@@ -5,7 +5,7 @@ public class PreviousGreatest {
         int[] stack = {15,10,18,12,4,6,2,8};
         prevGreater(stack);
     }
-    public static void prevGreater(int arr[]) {
+    public static void prevGreater(int[] arr) {
         for(int i = 0; i < arr.length; i++){
             int pg = -1;
             for(int j = i-1; j >= 0;j--) {
