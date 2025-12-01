ef79e88681386dc86b7bc6735049e58596faa0d1
Chndn <chndnsharma5036@gmail.com>
2025-12-01 23:07:46 +0530

    using Deque stack

--- Stacks/src/PreviousGreatest.java
+++ Stacks/src/PreviousGreatest.java
@@ -2,19 +2,21 @@ import java.util.*;
 
 public class PreviousGreatest {
     public static void main(String[] args){
-        int[] stack = {15,10,18,12,4,6,2,8};
+        int[] stack = {20,30,10,5,15};
         prevGreater(stack);
     }
     public static void prevGreater(int[] arr) {
-        for(int i = 0; i<arr.length; i++){
-            int pg = -1;
-            for(int j = i-1; j >= 0;j--) {
-                if (arr[j]>arr[i]){
-                    pg = arr[j];
-                    break;
-                }
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(arr[0]);
+        int pg = -1;
+        System.out.println(pg + " ");
+        for(int i = 1; i < arr.length; i++){
+            while(stack.isEmpty() == false && stack.peek() <= arr[i]){
+                stack.pop();
             }
-            System.out.print(pg + " ");
+            pg = stack.isEmpty() ? -1 : stack.peek();
+            System.out.println(pg + " ");
+            stack.push(arr[i]);
         }
     }
 }
