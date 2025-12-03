f1fad00017aa3c2b9f466afa8f344375e87f85a2
Chndn <chndnsharma5036@gmail.com>
2025-12-03 17:27:56 +0530

    using stack

--- Stacks/src/NextGreater.java
+++ Stacks/src/NextGreater.java
@@ -1,18 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class NextGreater {
     public static void main(String[] args){
         int[] arr = {5,15,10,8,6,12,7};
         nextGreater(arr);
     }
     public static void nextGreater(int[] arr){
-        for(int i = 0; i< arr.length; i++){
-            int ng = -1;
-            for(int j = i+1; j < arr.length; j++){
-                if(arr[j]> arr[i]){
-                    ng = arr[i];
-                    break;
-                }
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = arr.length;
+        stack.push(arr[n-1]);
+        int res[] = new int[n];
+        res[n-1] = -1;
+
+        for(int i=n-2;i>=0;i--){
+            while(stack.isEmpty() == false && stack.peek() <= arr[i]){
+                stack.pop();
             }
-            System.out.println(ng + " ");
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+        for(int i = 0; i<n; i++){
+            System.out.print(res[i] + " ");
         }
     }
 }
