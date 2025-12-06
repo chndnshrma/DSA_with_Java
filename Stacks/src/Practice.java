518227d25b6d17ba77612856619922618d50830f
Chndn <chndnsharma5036@gmail.com>
2025-12-06 17:28:48 +0530

    bug fixes

--- Stacks/src/Practice.java
+++ Stacks/src/Practice.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class Practice {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4,5};
 
         Stack<Integer> s = new Stack<>();
         for (int j: arr){
