f8c08198d5d568bb1ab5d04c9633a0ebca13b6c2
Chndn <chndnsharma5036@gmail.com>
2025-12-07 23:47:59 +0530

    some changes

--- Stacks/src/Practice.java
+++ Stacks/src/Practice.java
@@ -4,7 +4,7 @@ public class Practice {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4,5};
 
-        Stack<Integer> s = new Stack<>();
+        Stack<Integer> s =new Stack<>();
         for (int j: arr){
             s.push(j);
         }
