53704af5fa5546b07b7b2ef4ce9231b80d7faf3b
Chndn <chndnsharma5036@gmail.com>
2025-12-10 03:16:09 +0530

    achieved solution of problem to remove con duplicates using stack

--- Stacks/src/Practice.java
+++ Stacks/src/Practice.java
@@ -3,13 +3,30 @@ import java.util.Stack;
 public class Practice {
     public static void main(String[] args) {
     String str = "toramaikechodoaaaiiyeee";
-    removeConsecutiveDuplicates(str);
+    System.out.println(removeConsecutiveDuplicates(str));
     }
-
     public static String removeConsecutiveDuplicates(String s) {
-        Deque<Integer> stack =new ArrayDeque<>();
-        stack.push(s.toCharArray()) ;
+        // Your code here
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
 
+        int n = s.length();
 
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            char currentChar = s.charAt(i);
+
+            if (st.isEmpty() || st.peek() != currentChar) {
+                st.push(currentChar);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        while (!st.isEmpty()) {
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
     }
 }
