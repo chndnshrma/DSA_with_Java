025d6206b2064a37e7b54f8de25dc8edfc7add9d
Chndn <chndnsharma5036@gmail.com>
2025-12-10 18:07:06 +0530

    this is my solution

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+        q.offer(40);
+        q.offer(50);
 
+        removeElements(q,3);
+    }
+    public static void removeElements(Queue<Integer> q, int k) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < k; i++) {
+            stack.push(q.poll());
+        }
+
+        while(!stack.isEmpty()) {
+            q.offer(stack.pop());
+        }
+        for(int i = 0; i <= q.size() - k; i++){
+            stack.push(q.poll());
+
+        }
+        while(!stack.isEmpty()) {
+            q.offer(stack.pop());
+        }
+        for(int e : q){
+            System.out.print(e + " ");
+        }
     }
 }
