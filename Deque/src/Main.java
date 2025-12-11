914a92b678c570bc7ea78bcf33685dd3f1f909b3
Chndn <chndnsharma5036@gmail.com>
2025-12-12 02:12:18 +0530

    max in sliding windows problem solved

--- Deque/src/Main.java
+++ Deque/src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+        int arr[] = {20, 40, 30, 10, 60};
+        int k = 3;
+        printMax(arr, k);
+    }
+    static void printMax(int arr[], int k) {
+        Deque<Integer> dq = new LinkedList<Integer>();
 
+        // Process first K elements
+        for (int i = 0; i < k; i++) {
+            while (!dq.isEmpty() && arr[i] >= arr[dq.peekLast()])
+                dq.removeLast();
+
+            dq.addLast(i);
+        }
+
+        // Process remaining elements
+        for (int i = k; i < arr.length; i++) {
+            System.out.print(arr[dq.peek()] + " ");
+
+            // Remove elements that are out of this window
+            while (!dq.isEmpty() && dq.peek() <= i - k)
+                dq.removeFirst();
+
+            // Remove smaller elements from the back
+            while (!dq.isEmpty() && arr[i] >= arr[dq.peekLast()])
+                dq.removeLast();
+
+            dq.addLast(i);
+        }
+
+        // Print max of last window
+        System.out.print(arr[dq.peek()] + " ");
     }
 }
