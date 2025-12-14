9544d9da0450ebac06df58418998eb63f8a9f347
Chndn <chndnsharma5036@gmail.com>
2025-12-14 17:54:12 +0530

    solved sliding windows max problem

---
+++ Deque/src/SlidingWindowsMAx.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlidingWindowsMAx {
+    public static void main(String[] args){
+        int[] arr = {20,25,12,4,15,41,12};
+        int k = 3;
+        System.out.println(maxOfSubarrays(arr,k));
+    }
+    public static ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = arr.length;
+
+        for(int i = 0; i < n; i++){
+            while(!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+
+            if(dq.peekFirst() == i-k){
+                dq.removeFirst();
+            }
+            if(i >= k-1){
+                ans.add(arr[dq.peekFirst()]);
+            }
+        }
+        return ans;
+    }
+}
