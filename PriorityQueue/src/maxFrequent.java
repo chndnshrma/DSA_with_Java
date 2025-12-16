6fa65aa68e089e03b46dfa50ac76211afacfb701
Chndn <chndnsharma5036@gmail.com>
2025-12-16 18:23:23 +0530

    trying to solve the problem myself

---
+++ PriorityQueue/src/maxFrequent.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+public class maxFrequent {
+    public static void main(String[] args){
+        int[] arr = {10,5,20,5,10,10,30};
+        kmaxFrequent(arr, );
+    }
+    public static void kmaxFrequent(int[] arr, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < k; i++){
+            pq.add(arr[i]);
+        }
+    }
+}
