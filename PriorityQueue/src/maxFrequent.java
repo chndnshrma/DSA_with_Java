19cfb47d596d3dd902d7974efbadd5d53e649477
Chndn <chndnsharma5036@gmail.com>
2026-02-13 02:55:59 +0530

    to find k largest elements in a queue

--- PriorityQueue/src/maxFrequent.java
+++ PriorityQueue/src/maxFrequent.java
@@ -3,12 +3,23 @@ import java.util.Queue;
 
 public class maxFrequent {
     public static void main(String[] args){
-        Queue<Integer> q = new PriorityQueue<>();
-        q.add(1);
-        q.add(3);
-        q.add(3);
-        q.add(5);
-        System.out.println(q);
+        int[] arr = {10,20,13,41,28,45};
+        printK(arr, 2);
+    }
+    public static void printK(int[] arr, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < k; i++){
+            pq.add(arr[i]);
+        }
+        for(int i = k; i < arr.length; i++){
+            if(arr[i] > pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        for(int i = 0; i < k; i++){
+            System.out.println(pq.poll());
+        }
     }
 }
 
