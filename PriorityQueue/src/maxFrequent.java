7fc09fd560cd7aed6b191aa17278caed532a6479
Chndn <chndnsharma5036@gmail.com>
2026-02-13 02:39:38 +0530

    revising queue

--- PriorityQueue/src/maxFrequent.java
+++ PriorityQueue/src/maxFrequent.java
@@ -1,14 +1,14 @@
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class maxFrequent {
     public static void main(String[] args){
-        int[] arr = {10,5,20,5,10,10,30};
-        kmaxFrequent(arr, );
-    }
-    public static void kmaxFrequent(int[] arr,int k){
-        PriorityQueue<Integer> pq =new PriorityQueue<>();
-        for(int i =0;i < k;i++){
-            pq.add(arr[i]);
-        }
+        Queue<Integer> q = new PriorityQueue<>();
+        q.add(1);
+        q.add(3);
+        q.add(3);
+        q.add(5);
+        System.out.println(q);
     }
 }
+
