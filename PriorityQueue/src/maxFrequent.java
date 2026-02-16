a6b462231b4493d5deefa346369816eeff34e460
Chndn <chndnsharma5036@gmail.com>
2026-02-17 02:48:49 +0530

    update

--- PriorityQueue/src/maxFrequent.java
+++ PriorityQueue/src/maxFrequent.java
@@ -2,24 +2,25 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class maxFrequent {
-    public static void main(String[] args){
-        int[] arr = {10,20,13,41,28,45};
-        printK(arr, 2);
+    public static void main(String[] args) {
+        int[] arr = {13,20,32,54,67,43,90};
+        kLargestElement(arr, 3);
     }
-    public static void printK(int[] arr, int k){
+    public static void kLargestElement(int[] arr, int k){
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0; i < k; i++){
-            pq.add(arr[i]);
+        for(int i = 0;i<k; i++){
+            pq.add(i);
         }
-        for(int i = k; i < arr.length; i++){
+        for(int i = k; i<arr.length; i++){
             if(arr[i] > pq.peek()){
                 pq.poll();
                 pq.add(arr[i]);
             }
         }
-        for(int i = 0; i < k; i++){
-            System.out.println(pq.poll());
+        for(int i = 0; i<k; i++){
+            System.out.print(pq.poll()+ " ");
         }
+
     }
 }
 
