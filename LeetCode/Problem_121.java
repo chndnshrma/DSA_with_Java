143b67226f07ce3fdc0cc4d2eff4f6d7c167d5d3
Chndn <chndnsharma5036@gmail.com>
2026-04-10 02:23:21 +0530

    startin my leetcode journey

---
+++ LeetCode/Problem_121.java
@@ -0,0 +1,14 @@
+public class Problem_121 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(solution.maxProfit(prices));
+    }
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        
+    }
+}
