b68768619f394a9d469cce4fa2f590d644825e0e
Chndn <chndnsharma5036@gmail.com>
2026-04-10 02:23:21 +0530

    startin my leetcode journey

---
+++ Practice/src/MyRunnable.java
@@ -0,0 +1,19 @@
+public class MyRunnable implements Runnable{
+    private final String text;
+
+    public MyRunnable(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 1;i<=5;i++){
+            try{
+               Thread.sleep(1000);
+                System.out.println(text);
+            }catch(InterruptedException e){
+                System.out.println("Thread was interrupted!");
+            }
+        }
+    }
+}
