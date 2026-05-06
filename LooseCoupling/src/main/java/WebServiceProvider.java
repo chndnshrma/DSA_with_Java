89585b367177a8c4077efbfe379ad5de4bf4ae61
Chndn <chndnsharma5036@gmail.com>
2026-05-06 23:54:23 +0530

    loose coupiling example

---
+++ LooseCoupling/src/main/java/WebServiceProvider.java
@@ -0,0 +1,7 @@
+public class WebServiceProvider implements UserDataProvider{
+
+    @Override
+    public String userDataProvider() {
+        return "Data Recieved from WebServiceProvider";
+    }
+}
