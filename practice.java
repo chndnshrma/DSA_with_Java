f06403604106402cfe7c7044a44b1638e7484e70
Chndn <chndnsharma5036@gmail.com>
2026-09-03 02:28:08 +0530

    learnt selection sort

--- practice.java
+++ practice.java
@@ -1,5 +1,27 @@
 class practice {
     public static void main(String[] args) {
-       System.out.println("Hello");
+       int[] arr = {15, 9, 20, 3, 56, 42, 12};
+        int n = arr.length;
+
+        for (Integer e : arr) {
+        System.out.print(e + " ");
+       }
+       System.out.println();
+
+       for (int i = 0; i <= n-2; i++) {
+        int min = i;
+        for ( int j = i; j <= n-1; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp; 
+       }
+       for (Integer e : arr) {
+        System.out.print(e + " ");
+       }
+       System.out.println();
     }
 }
