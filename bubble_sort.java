f2084263ba5aa486f6114a0a404642753859d189
Chndn <chndnsharma5036@gmail.com>
2026-09-04 17:10:15 +0530

    optimized bubble sort for best time complexity

--- bubble_sort.java
+++ bubble_sort.java
@@ -16,13 +16,19 @@ public class bubble_sort {
     }
     public static void bubbleSort(int[] arr, int n) {
         for (int i = n-1; i >= 0; i--) {
+            int didSwap = 0;
             for (int j = 0; j <= i-1; j++ ) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
+                    didSwap = 1;
                 }
             }
+            if (didSwap == 0) {
+                break;
+            }
+            System.out.println("Runs \n");
         }
     }
 }
