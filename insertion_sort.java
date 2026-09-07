a68c1e6e5cc2cd4fccbacdddfbfd5c00c9992617
Chndn <chndnsharma5036@gmail.com>
2026-09-08 00:43:21 +0530

    checked for second largest element in an array

--- insertion_sort.java
+++ insertion_sort.java
@@ -21,6 +21,7 @@ public class insertion_sort {
                 int temp = arr[j-1];
                 arr[j-1] = arr[j];
                 arr[j] = temp;
+                
                 j--;
             }
         }
--- patterns.java
+++ patterns.java
@@ -1,102 +1,31 @@
-public class patterns {
+class patterns {
     public static void main(String[] args) {
-        printPattern10();
-        
-    }
-    public static void printPattern1() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j< 5; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
+        int[] arr = {15, 9, 20, 3, 56, 42, 12};
+        int n = arr.length;
+
+        for (Integer e : arr) {
+        System.out.print(e + " ");
         }
+       System.out.println();
+
+       System.out.println("Second Largest Element: " + secondLargest(arr));
     }
 
-    public static void printPattern2() {
-        for (int i = 0; i< 5; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        } 
-    }
-
-    public static void printPattern3() {
-        for (int i = 1; i <= 5; i++) {
-            for(int j = 1; j <= i; j++) {
-                System.out.print (j + " ");
-            }
-            System.out.println();
+    static int secondLargest(int[] arr) {
+        if ( arr == null || arr.length < 2) {
+            return -1;
         }
-    }
+        int largestElement = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
-    public static void printPattern4() {
-        for (int i = 1; i <= 5; i++) {
-            for(int j = 1; j <= i; j++) {
-                System.out.print (i + " ");
+        for (int value : arr) {
+            if (value > largestElement) {
+                secondLargest = largestElement;
+                largestElement = value;
+            } else if (value > secondLargest && value != largestElement) {
+                secondLargest = value;
             }
-            System.out.println();
         }
-    }
-    //row - i + 1
-    public static void printPattern5() {
-        for (int i = 1; i <= 5; i++) {
-            for(int j = 0; j < 5-i+1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printPattern6() {
-        for (int i = 1; i <= 5; i++) {
-            for(int j = 1; j <= 5-i+1; j++) {
-                System.out.print(j+ " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void printPattern7() {
-        for (int i = 0; i< 5; i++) {
-            for (int j = 0; j < 5 - i -1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < 2*i+1; j++) {
-                System.out.print("*");
-            }
-            for (int j = 0; j < 5 - i -1; j++) {
-                System.out.print(" ");
-            }
-            System.out.println();
-        }    
-    }
-
-    public static void printPattern8() {
-        for (int i = 0; i< 5; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < 2*5 - (2*i+1); j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }    
-    }
-
-    public static void printPattern9() {
-        printPattern7();
-        printPattern8();   
-    }
-
-    public static void printPattern10() {
-        for (int i = 1; i <= 2*5-1;i ++) {
-            int stars = i;
-            if (i > 5) stars = 2*5 - i;
-            for (int j = 1; j <= stars ; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        return secondLargest;
     }
 }
