a7fbd1d87e5d1054d1dccc44a47e256d9e7509f6
Chndn <chndnsharma5036@gmail.com>
2026-09-09 01:18:14 +0530

    removed duplicate elemtn

--- patterns.java
+++ patterns.java
@@ -1,31 +1,25 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 class patterns {
     public static void main(String[] args) {
-        int[] arr = {15, 9, 20, 3, 56, 42, 12};
-        int n = arr.length;
-
-        for (Integer e : arr) {
-        System.out.print(e + " ");
+        int[] arr = {1,1,2,2,3,3,4,4,5,5,5};
+        for (int e : arr) {
+            System.out.print(e + " ");
         }
-       System.out.println();
+        System.out.println();
 
-       System.out.println("Second Largest Element: " + secondLargest(arr));
+        System.out.println(removeDuplicate(arr));
+
     }
-
-    static int secondLargest(int[] arr) {
-        if ( arr == null || arr.length < 2) {
-            return -1;
-        }
-        int largestElement = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
-
-        for (int value : arr) {
-            if (value > largestElement) {
-                secondLargest = largestElement;
-                largestElement = value;
-            } else if (value > secondLargest && value != largestElement) {
-                secondLargest = value;
+    public static int removeDuplicate(int[] arr) {
+        int i = 0;
+        for ( int j = 1; j < arr.length; j++){
+            if (arr[j] != arr[i]) {
+                i++;
+                arr[i] = arr[j];
             }
         }
-        return secondLargest;
+        return i + 1;
     }
 }
