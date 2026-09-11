85eb38e940b47735a291c647c99b35e52fe2922a
Chndn <chndnsharma5036@gmail.com>
2026-09-11 23:36:21 +0530

    some changes

--- patterns.java
+++ patterns.java
@@ -5,11 +5,11 @@ class patterns {
     public static void main(String[] args) {
         int[] arr = {1,1,2,2,3,3,4,4,5,5,5};
         for (int e : arr) {
-            System.out.print(e + " ");
+            System.out.print(e + "");
         }
         System.out.println();
     }
     public static int[] rotateArray(int[] arr, int k) {
-        
+        retutn int k;
     }
 }
