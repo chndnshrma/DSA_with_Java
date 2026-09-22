e2ace779558f5412ee73c5ac05e86fc15b8e38e7
Chndn <chndnsharma5036@gmail.com>
2026-09-23 04:03:45 +0530

    roated an array by d plcaes

--- patterns.java
+++ patterns.java
@@ -1,11 +1,11 @@
 class patterns {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,2,3,4,5,6,7,8,9,0};
         for (int e : arr) {
             System.out.print(e + " ");
         }
         System.out.println();
-        rotateArray(arr);
+        rotateArray(arr, 3);
 
         for (int e : arr) {
             System.out.print(e + " ");
@@ -13,14 +13,23 @@ class patterns {
         System.out.println();
 
     }
-    public static int[] rotateArray(int[] arr) {
-        int temp = arr[0];
-        int k = 2;
+    public static int[] rotateArray(int[] arr, int d) {
         int n = arr.length;
-        for (int i = 1; i<n; i++) {
-            arr[i-2] = arr[i];
+        d = d % n;
+        int[] temp = new int[d];
+        
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
         }
-        arr[n-2] = temp;
+
+        for (int i = d; i < n ; i++) {
+            arr[i-d] = arr[i];
+        }
+
+        for (int i = n-d; i < n; i++) {
+            arr[i] = temp[i - (n-d)];
+        }
+
         return arr;
     }
 }
