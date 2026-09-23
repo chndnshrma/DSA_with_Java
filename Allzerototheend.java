adc9d2ae111c736787eeb18e13f1542b796b5601
Chndn <chndnsharma5036@gmail.com>
2026-09-24 04:14:08 +0530

    optimal solution to move all zeros to the end in an array

--- Allzerototheend.java
+++ Allzerototheend.java
@@ -15,31 +15,56 @@ class Allzerototheend {
         }
         System.out.println();
     }
+    //optimal solution
     public static int[] moveZeroToEnd(int[] arr) {
-        // if (arr == null || arr.length == 0) return;
-
         int n = arr.length;
-        int nz = 0;
-
-        for (int e : arr) {
-            if (e != 0) {
-                nz++;
-            }
-        }
-        
-        List<Integer> temp = new ArrayList<>();
+        int j = -1;
         for (int i = 0; i<n; i++) {
-            if (arr[i] != 0) {
-                temp.add(arr[i]);
+            if (arr[i] == 0) {
+                j = i;
+                break;
             }
         }
+        if (j == -1) return arr;
 
-        for (int i = 0; i < temp.size(); i++) {
-            arr[i] = temp.get(i);
-        }
-        for (int i = nz; i<n; i++) {
-            arr[i] = 0;
+        for (int i = j+1; i < n ;i++) {
+            if (arr[i] != 0) {
+                swap(arr, i, j);
+                j++;
+            }
         }
         return arr;
     }
+    public static void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+    //brutefor solution
+    // public static int[] moveZeroToEnd(int[] arr) {
+
+    //     int n = arr.length;
+    //     int nz = 0;
+
+    //     for (int e : arr) {
+    //         if (e != 0) {
+    //             nz++;
+    //         }
+    //     }
+        
+    //     List<Integer> temp = new ArrayList<>();
+    //     for (int i = 0; i<n; i++) {
+    //         if (arr[i] != 0) {
+    //             temp.add(arr[i]);
+    //         }
+    //     }
+
+    //     for (int i = 0; i < temp.size(); i++) {
+    //         arr[i] = temp.get(i);
+    //     }
+    //     for (int i = nz; i<n; i++) {
+    //         arr[i] = 0;
+    //     }
+    //     return arr;
+    // }
 }
