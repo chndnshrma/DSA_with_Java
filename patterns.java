f81a821696879d3bc1908e995b22fbda4aa0b9c0
Chndn <chndnsharma5036@gmail.com>
2026-09-24 02:01:18 +0530

    learning rotating array

--- patterns.java
+++ patterns.java
@@ -1,6 +1,6 @@
 class patterns {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,0};
+        int[] arr = {1,2,3,4,5,6,7};
         for (int e : arr) {
             System.out.print(e + " ");
         }
@@ -31,5 +31,26 @@ class patterns {
         }
 
         return arr;
+    }
+}
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0) {
+            return;
+        }
+        k = k % n;
+        int[] temp = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
     }
 }
