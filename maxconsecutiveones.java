b738769eb929510ac9eebed30300133f01c237f5
Chndn <chndnsharma5036@gmail.com>
2026-09-26 02:30:31 +0530

    some changes

---
+++ maxconsecutiveones.java
@@ -0,0 +1,22 @@
+public class maxconsecutiveones {
+    public static void main(String[] args) {
+        int[] arr = {1,0,3,0,5,0,7};
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+        consecutiveOnes(arr);
+
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+    public static int consecutiveOnes(int[] arr) {
+        int max = 0;
+        for (int i = 0; i<arr.length; i++) {
+            
+        }
+        return max;
+    }
+}
